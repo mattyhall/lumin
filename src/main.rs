bf2496bcd99cf180ebf481f7dae7ca7cccd25911
Matt Hall <matthew@quickbeam.me.uk>
2023-07-16 13:55:53 +0100

    Move processing into its own file
    Also move the store's lock into the actual `Store` struct

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,22 @@
-use axum::http::{header, Request};
+mod store;
+
+use axum::http::Request;
 use axum::response::IntoResponse;
 use axum::routing::get;
 use axum::{Extension, Router};
-use std::collections::HashMap;
 use std::error::Error;
-use std::io::Read;
 use std::net::SocketAddr;
-use std::path::{Path, PathBuf};
 use std::sync;
+use store::{find_and_process, Store};
 use tower::ServiceBuilder;
 use tower_http::trace::TraceLayer;
-use tracing::{debug, info, instrument};
-
-const EXTENSIONS: &[&str] = &["css", "html", "jpg", "jpeg", "woff2"];
-
-#[derive(Clone)]
-struct Resource {
-    path: PathBuf,
-    contents: Vec<u8>,
-}
-
-impl Resource {
-    fn content_type(&self) -> String {
-        mime_guess::from_path(&self.path)
-            .first_or_text_plain()
-            .essence_str()
-            .to_owned()
-    }
-}
-
-impl IntoResponse for Resource {
-    fn into_response(self) -> axum::response::Response {
-        let typ = self.content_type();
-        let res = ([(header::CONTENT_TYPE, typ)], self.contents);
-        res.into_response()
-    }
-}
-
-trait ResourceProcessor {
-    fn matches(path: &Path) -> bool;
-    fn process(path: &Path) -> Result<Resource, Box<dyn Error>>;
-}
-
-struct StaticProcessor {}
-impl ResourceProcessor for StaticProcessor {
-    fn matches(path: &Path) -> bool {
-        path.extension()
-            .map(|e| EXTENSIONS.iter().any(|wanted| *wanted == e))
-            .unwrap_or(false)
-    }
-
-    #[instrument]
-    fn process(path: &Path) -> Result<Resource, Box<dyn Error>> {
-        info!("statically processing");
-
-        let mut buf = Vec::new();
-        std::fs::File::open(path)?.read_to_end(&mut buf)?;
-
-        Ok(Resource {
-            path: path.to_owned(),
-            contents: buf,
-        })
-    }
-}
-
-#[derive(Default, Clone)]
-struct Store {
-    hm: HashMap<PathBuf, Resource>,
-}
-
-impl Store {
-    fn put<P: Into<PathBuf>>(self: &mut Self, path: P, resource: Resource) {
-        let path = path.into();
-        debug!(
-            ?path,
-            content_length = resource.contents.len(),
-            "putting into store"
-        );
-        self.hm.insert(path, resource);
-    }
-
-    fn get<P: AsRef<Path>>(self: &Self, path: P) -> Option<Resource> {
-        self.hm.get(path.as_ref()).cloned()
-    }
-}
-
-fn walk(base: &Path, output: &mut Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
-    for entry in std::fs::read_dir(base)? {
-        let entry = entry?;
-        let path = entry.path();
-        if entry.metadata()?.is_dir() {
-            walk(&path, output)?;
-            continue;
-        }
-
-        match path.extension() {
-            Some(ext) if EXTENSIONS.iter().any(|wanted| *wanted == ext) => {}
-            _ => continue,
-        }
-
-        debug!(?path, "Found resource");
-
-        output.push(path);
-    }
-
-    Ok(())
-}
-
-fn find_and_process() -> Result<Store, Box<dyn Error>> {
-    let mut paths = Vec::new();
-    let cwd = std::env::current_dir()?;
-
-    walk(&cwd, &mut paths)?;
-
-    let mut store = Store::default();
-
-    for path in paths {
-        if !StaticProcessor::matches(&path) {
-            break;
-        }
-
-        let short_path = path.strip_prefix(&cwd)?;
-        store.put(short_path, StaticProcessor::process(&path)?);
-    }
-
-    Ok(store)
-}
+use tracing::info;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt::init();
 
-    let store = sync::Arc::new(sync::Mutex::new(find_and_process()?));
+    let store = sync::Arc::new(find_and_process()?);
 
     let app = Router::new().fallback(get(root)).layer(
         ServiceBuilder::new()
@@ -148,11 +33,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-async fn root<T>(
-    store: Extension<sync::Arc<sync::Mutex<Store>>>,
-    request: Request<T>,
-) -> impl IntoResponse {
-    let handle = store.0.lock().unwrap();
+async fn root<T>(store: Extension<sync::Arc<Store>>, request: Request<T>) -> impl IntoResponse {
     let path = request.uri().path().trim_start_matches('/');
-    handle.get(path).unwrap().to_owned()
+    store.get(path).unwrap().to_owned()
 }
---
+++ src/store.rs
@@ -0,0 +1,133 @@
+use axum::http::header;
+use axum::response::IntoResponse;
+use std::collections::HashMap;
+use std::error::Error;
+use std::io::Read;
+use std::path::{Path, PathBuf};
+use std::sync;
+use tracing::{debug, info, instrument};
+
+const EXTENSIONS: &[&str] = &["css", "html", "jpg", "jpeg", "woff2"];
+
+#[derive(Clone)]
+pub struct Resource {
+    path: PathBuf,
+    contents: Vec<u8>,
+}
+
+impl Resource {
+    fn content_type(&self) -> String {
+        mime_guess::from_path(&self.path)
+            .first_or_text_plain()
+            .essence_str()
+            .to_owned()
+    }
+}
+
+impl IntoResponse for Resource {
+    fn into_response(self) -> axum::response::Response {
+        let typ = self.content_type();
+        let res = ([(header::CONTENT_TYPE, typ)], self.contents);
+        res.into_response()
+    }
+}
+
+trait ResourceProcessor {
+    fn matches(path: &Path) -> bool;
+    fn process(path: &Path) -> Result<Resource, Box<dyn Error>>;
+}
+
+struct StaticProcessor {}
+impl ResourceProcessor for StaticProcessor {
+    fn matches(path: &Path) -> bool {
+        path.extension()
+            .map(|e| EXTENSIONS.iter().any(|wanted| *wanted == e))
+            .unwrap_or(false)
+    }
+
+    #[instrument]
+    fn process(path: &Path) -> Result<Resource, Box<dyn Error>> {
+        info!("statically processing");
+
+        let mut buf = Vec::new();
+        std::fs::File::open(path)?.read_to_end(&mut buf)?;
+
+        Ok(Resource {
+            path: path.to_owned(),
+            contents: buf,
+        })
+    }
+}
+
+#[derive(Default)]
+pub struct Store {
+    hm: sync::Mutex<HashMap<PathBuf, Resource>>,
+}
+
+impl Store {
+    fn put<P: Into<PathBuf>>(self: &mut Self, path: P, resource: Resource) {
+        let path = path.into();
+        info!(
+            ?path,
+            content_length = resource.contents.len(),
+            "putting into store"
+        );
+
+        let mut hm = self.hm.lock().unwrap();
+        hm.insert(path, resource);
+    }
+
+    pub fn get<P: AsRef<Path>>(self: &Self, path: P) -> Option<Resource> {
+        let hm = self.hm.lock().unwrap();
+        hm.get(path.as_ref()).cloned()
+    }
+}
+
+fn walk(base: &Path, output: &mut Vec<PathBuf>) -> Result<(), Box<dyn Error>> {
+    for entry in std::fs::read_dir(base)? {
+        let entry = entry?;
+        let path = entry.path();
+        if entry.metadata()?.is_dir() {
+            walk(&path, output)?;
+            continue;
+        }
+
+        match path.extension() {
+            Some(ext) if EXTENSIONS.iter().any(|wanted| *wanted == ext) => {}
+            _ => continue,
+        }
+
+        debug!(?path, "Found resource");
+
+        output.push(path);
+    }
+
+    Ok(())
+}
+
+fn find_and_process() -> Result<Store, Box<dyn Error>> {
+    let start = std::time::Instant::now();
+
+    info!("rebuilding");
+
+    let mut paths = Vec::new();
+    let cwd = std::env::current_dir()?;
+
+    walk(&cwd, &mut paths)?;
+
+    let mut store = Store::default();
+
+    for path in paths {
+        if !StaticProcessor::matches(&path) {
+            break;
+        }
+
+        let short_path = path.strip_prefix(&cwd)?;
+        store.put(short_path, StaticProcessor::process(&path)?);
+    }
+
+    info!(elapsed=?start.elapsed(), "rebuilding finished");
+
+    Ok(store)
+}
+
